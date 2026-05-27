1e83938f591337868f8406ed6dfacfa08c0426ff
saya1805 <sayalishelke000@gmail.com>
2026-05-27 16:16:11 +0530

    add responsive changes & ui changes

--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -9,23 +9,33 @@ import { Component } from '@angular/core';
 })
 export class FrontPageComponent {
 
-  private readonly targetSectionId = 'workExperienceSection';
 
   constructor(){}
 
   ngOninit(){}
 
   scrollToWork() {
-    this.scrollToSection(this.targetSectionId);
+    this.scrollToSection('workExperienceSection');
   }
 
-  // स्क्रोलिंग लॉजिक (हा फंक्शन थेट id वापरून स्क्रोल करेल)
+  scrolltoproject(){
+    this.scrollToSection('ProjectWorkSection');
+  }
+
+  scrolltoprojectmodern(){
+    this.scrollToSection('ProjectWorkSectionmodern');
+  }
+
+  scrolltoprojecttech(){
+    this.scrollToSection('ProjectWorkSectiontech');
+  }
+
   scrollToSection(sectionId: string) {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ 
-        behavior: 'smooth', // ⬅️ स्मूथ ॲनिमेशनसाठी
-        block: 'start'      // ⬅️ सेक्शन स्क्रीनच्या वरच्या बाजूला दिसेल
+        behavior: 'smooth', 
+        block: 'start'    
       });
     }
   }
--- src/app/services/array-service.service.ts
+++ src/app/services/array-service.service.ts
@@ -43,13 +43,23 @@ export class ArrayServiceService {
 
   workExperienceskill=[
     {
-      'project':"Mconnect NASA Loan Management System",
+      'project':"Concent Management System",
       'pSummary':"Core Angular Front-End and PDF Reporting Application for SBI Life Insurance.",
       'performace':"98",
       'Accessibilty':"100",
       'Matrix':"System Stability",
       'matrixnumber':"100",
       'border':"#0b97da",
+      'Technical':"The Mconnect Consent & Grievance Management System is an enterprise-grade extension built within the existing Mconnect ecosystem. It is designed to digitize, secure, and streamline the end-user consent lifecycle and provide a robust mechanism for handling consumer grievances (complaints). The system features a distinct split architecture consisting of a Public-Facing Customer Portal (triggered via SMS automation) and a Secure Admin Dashboard protected by token-based authentication."
+    },
+    {
+      'project':"Mconnect NASA Loan Management System",
+      'pSummary':"Core Angular Front-End and PDF Reporting Application for SBI Life Insurance.",
+      'performace':"95",
+      'Accessibilty':"100",
+      'Matrix':"System Stability",
+      'matrixnumber':"100",
+      'border':"#029002",
       'Technical':"Developed and integrated robust APIs using .NET Core.Directly interacted with the SQL Database for efficient data fetching and management.Used integrated Full-Stack ownership to diagnose and resolve cross-layer issues promptly.Responsible for Unit testing, project building, and IIS server publishing/deployment."
     },
     {
@@ -59,7 +69,7 @@ export class ArrayServiceService {
       'Accessibilty':"100",
       'Matrix':"System Stability",
       'matrixnumber':"100",
-      'border':"#029002",
+      'border':"#a159ff",
       'Technical':"Performed precise calculation of tank measurements using a specific formula.Implemented Robust Authorization and Authentication (including automatic email/OTP generation).Utilized a SQL database for data storage and supported flexible database migration."
     },
     {
@@ -69,7 +79,7 @@ export class ArrayServiceService {
       'Accessibilty':"100",
       'Matrix':"System Stability",
       'matrixnumber':"100",
-      'border':"#a159ff",
+      'border':"#0b97da",
       'Technical':"Effective use of core Angular features like Reactive Forms and HTTP Client API integration.Designed Responsive and Maintainable layouts using HTML grids.Developed key features including a feature-rich email application (with spam filtering), a B2B e-commerce platform (for product management), and a chat application."
     },
     {
@@ -79,7 +89,7 @@ export class ArrayServiceService {
       'Accessibilty':"100",
       'Matrix':"System Stability",
       'matrixnumber':"100",
-      'border':"#0b97da",
+      'border':"#029002",
       'Technical':"Proficient in leveraging Angular's Reactive Forms and HTTP Client to architect scalable front-end systems, featuring a high-performance B2B E-commerce engine, real-time Chat modules, and an intelligent Email client with spam filtering."
     }
   ]
